266783f3f02e32efdc41d7efeab2e67e19d6301f
Devarshi2285 <134229662+Devarshi2285@users.noreply.github.com>
2025-07-04 19:26:59 +0530

    🟥 RED: Add test for null input case

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,2 +1,15 @@
-public class StringCalculatorTest {
+import com.example.Calculator;
+import com.example.StringCalculator;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class StringCalculatorTest {
+
+        @Test
+        void nullStringShouldReturnZero() {
+            StringCalculator stringCalculator = new StringCalculator();
+            assertEquals(0,stringCalculator.add(""));
+        }
+
+
 }
